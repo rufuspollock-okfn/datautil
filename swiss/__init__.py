e9782a2ae1bb4a9ecbeedb735029625052d73554
rgrp <none@none>
2009-08-17 12:04:30 +0100

    [doc][s]: add CHANGELOG to main docstring.

--- swiss/__init__.py
+++ swiss/__init__.py
@@ -9,10 +9,32 @@ The swiss package provides various utilities for working with data:
   * Cleaning up and parsing data.
     * misc and date modules
 
+CHANGELOG
+=========
+
+v0.2 2009-08-??
+---------------
+
+  * Extensive refactoring of tabular module/package
+    * Standardized interface with BaseReader and BaseWriter
+    * JsonReader and JsonWriter providing json reading and writing
+    * TxtWriter to support writing to plain text
+  * Improvements to date parsing (e.g. support for circa 'c.')
+  * New id module to do 'compression' of uuids using 32 and 64 bit encoding
+
+
+v0.1 2009-06-03
+---------------
+
+  * Bring together existing code (from last 2+ years) into new 'swiss' package
+  * Url caching and scraping
+  * Tabular data handling including csv reader/writer, xls reader, latex writer
+    and associated utilities (such as pivot_table)
+  * Cleaning and parsing data especially dates (misc and date modules)
 '''
 __version__ = '0.2a'
 
-from tabular import *
+import tabular
 from cache import *
 from misc import *
 from id import *
