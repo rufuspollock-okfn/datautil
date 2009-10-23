26c2b4cfb8ffa4345d83148c4f30aa948f3b646b
rgrp <none@none>
2009-10-23 11:38:11 +0100

    [doc][xs]: fix up docstring do render better on pypi.

--- swiss/__init__.py
+++ swiss/__init__.py
@@ -1,12 +1,18 @@
-'''Swiss Army Knife for Data Work.
+'''Swiss Army Knife for Data Work
+==============================
 
 The swiss package provides various utilities for working with data:
 
   * Url caching and scraping
+
     * cache module
+
   * Processing and transforming tabular data to and from various formats.
+
     * tabular module
+
   * Cleaning up and parsing data.
+
     * misc and date modules
 
 CHANGELOG
