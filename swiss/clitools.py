63be828bb08f542bd75fb61e164a442983fc9401
rgrp <none@none>
2009-11-23 12:26:37 +0000

    [clitools][m]: (old) work over last several months on methods to expose at the cli all methods/functions on an object/module.

---
+++ swiss/clitools.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import optparse
+import inspect
+
+def _object_methods(obj):
+    methods = inspect.getmembers(obj, inspect.ismethod)
+    methods = filter(lambda (name,y): not name.startswith('_'), methods)
+    methods = dict(methods)
+    return methods
+
+def _module_functions(functions):
+    local_functions = dict(functions)
+    for k,v in local_functions.items():
+        if not inspect.isfunction(v) or k.startswith('_'):
+            del local_functions[k]
+    return local_functions
+
+def _main(functions_or_object):
+    isobject = inspect.isclass(functions_or_object)
+    if isobject:
+        _methods = _object_methods(functions_or_object)
+    else:
+        _methods = _module_functions(functions_or_object)
+
+    usage = '''%prog {action}
+
+    '''
+    usage += '\n    '.join(
+        [ '%s: %s' % (name, m.__doc__.split('\n')[0] if m.__doc__ else '') for (name,m)
+        in _methods.items() ])
+    parser = optparse.OptionParser(usage)
+    # Optional: for a config file
+    # parser.add_option('-c', '--config', dest='config',
+    #         help='Config file to use.')
+    options, args = parser.parse_args()
+
+    if not args or not args[0] in _methods:
+        parser.print_help()
+        sys.exit(1)
+
+    method = args[0]
+    if isobject:
+        getattr(functions_or_object, method)()
+    else:
+        _methods[method]()
+
+
+if __name__ == '__main__':
+    _main()
+
