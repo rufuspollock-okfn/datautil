678e204032bfbba3b21f2eb0559f0b554000429a
rgrp <http://rufuspollock.org>
2009-12-15 10:37:22 +0000

    [cache][m]: Progress counter for downloading.

--- swiss/cache.py
+++ swiss/cache.py
@@ -7,10 +7,40 @@ that do not require any other parts of the swiss package.
 import urlparse
 import urllib
 import os
+import sys
+
+
+# have to define before Cache as used in classmethod
+class _Progress(object):
+    def __init__(self):
+        self.count = -1
+
+    def dl_progress(self, count, block_size, total_size):
+        if self.count == -1:
+            print 'Total size: %s' % self.format_size(total_size)
+        last_percent = int(self.count*block_size*100/total_size)
+        percent = int(count*block_size*100/total_size)
+        if percent > last_percent:
+            # TODO: is this acceptable? Do we want to do something nicer?
+            sys.stdout.write('.')
+            sys.stdout.flush()
+        self.count = count
+
+    def format_size(self, bytes):
+        if bytes > 1000*1000:
+            return '%.1fMb' % (bytes/1000.0/1000)
+        elif bytes > 10*1000:
+            return '%iKb' % (bytes/1000)
+        elif bytes > 1000:
+            return '%.1fKb' % (bytes/1000.0)
+        else:
+            return '%ibytes' % bytes
+
 
 class Cache(object):
     '''A local file cache (and url retriever).
     '''
+
     def __init__(self, path='.'):
         '''
         @param path: path to cache (defaults to current directory)
@@ -23,7 +53,7 @@ class Cache(object):
         '''Retrieve url into cache and return the local path to it.'''
         dest = self.cache_path(url)
         if not os.path.exists(dest) or force:
-            self.dl(url, dest)
+            self.download(url, dest)
         return dest
 
     def cache_path(self, url):
@@ -45,10 +75,17 @@ class Cache(object):
         return result
 
     @classmethod
-    def dl(self, url, dest=None):
+    def download(self, url, dest=None):
         '''Download a file from a url.
         '''
         if not dest:
             dest = self.basename(url)
-        urllib.urlretrieve(url, dest)
+        print 'Retrieving %s' % url 
+        prog = _Progress()
+        urllib.urlretrieve(url, dest, reporthook=prog.dl_progress)
+
+    # for backwards compatability
+    @classmethod
+    def dl(self, url, dest=None):
+        return self.download(url, dest)
 
