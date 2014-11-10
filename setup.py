3f602a4c8004288301e3826f89a08a564961f7f0
Valentin Lorentz <progval+github@progval.net>
2014-11-10 13:13:06 +0100

    Mark in classifiers that only Python 2 is supported.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
